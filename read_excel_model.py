29cca45992d42ba03ee1fa1f7a4968091ae2e15e
nnursimulu <anavrin.ulumisrun@gmail.com>
2023-12-13 21:59:25 -0500

    added metabolite obj

--- read_excel_model.py
+++ read_excel_model.py
@@ -1,6 +1,38 @@
 import pandas as pd
 from cobra import Model, Reaction, Metabolite
 
+def get_rxn_dict(rxn, met_name_to_object):
+
+    reaction_dict = {}
+    #TODO
+    # reaction.add_metabolites({
+    # malACP_c: -1.0,
+    # h_c: -1.0,
+    # ddcaACP_c: -1.0,
+    # co2_c: 1.0,
+    # ACP_c: 1.0,
+    # omrsACP_c: 1.0
+    # })
+    return reaction_dict
+
+
+def get_met_name_to_object(open_sheet):
+    """Return metabolite name to a Metabolite object that has the information for that metabolite."""
+
+    met_name_to_object = {}
+
+    names = open_sheet["Abbreviation"]
+    descrips = open_sheet["Description"]
+    comps = open_sheet["Compartment"]
+    formulae = open_sheet["Neutral formula"]
+    
+    for name, descrip, comp, formula in zip(names, descrips, comps, formulae):
+        met_obj = Metabolite(name, formula=formula, name=descrip, compartment=comp)
+        met_name_to_object[name] = met_obj
+
+    return met_name_to_object
+
+
 def get_cobrapy_model(excel_file, model_id=None):
     """Return cobrapy model given contents of excel file, and name model (optional)"""
 
@@ -12,8 +44,7 @@ def get_cobrapy_model(excel_file, model_id=None):
     xlsx = pd.ExcelFile(excel_file)
     rxn_sheet = pd.read_excel(xlsx, "Reaction List")
     met_sheet = pd.read_excel(xlsx, "Metabolite List")
-    met_name_to_object = get_met_name_to_object(met_sheet) #TODO
-    # print (rxn_sheet)
+    met_name_to_object = get_met_name_to_object(met_sheet)
 
     names = rxn_sheet["Abbreviation"]
     descrips = rxn_sheet["Description"]
@@ -36,7 +67,8 @@ def get_cobrapy_model(excel_file, model_id=None):
         reaction.upper_bound = ub
         reaction.gene_reaction_rule = gpr
 
-        reaction_dict = get_rxn_dict(rxn) #TODO
+        reaction_dict = get_rxn_dict(rxn, met_name_to_object) #TODO
+        reaction.add_metabolites(reaction_dict)
         # Then, create the formula and add the reaction to the model.
         # https://cobrapy.readthedocs.io/en/latest/building_model.html
 
